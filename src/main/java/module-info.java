24b5787d9e5ee2a19aa562d46e185c3a2f50612f
Daniel <daniel.huson@uni-tuebingen.de>
2024-01-30 17:46:57 +0100

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,39 @@
+module jloda_fx {
+	requires transitive javafx.controls;
+	requires transitive javafx.graphics;
+	requires transitive javafx.fxml;
+
+	requires transitive java.desktop;
+
+	requires transitive jloda_core;
+	requires org.apache.fontbox;
+	requires org.apache.pdfbox;
+
+	exports jloda.fx.dialog;
+	exports jloda.fx.colorscale;
+	exports jloda.fx.control;
+	exports jloda.fx.control.table;
+	exports jloda.fx.control.sliderhistogram;
+
+	exports jloda.fx.find;
+	exports jloda.fx.graph;
+	exports jloda.fx.label;
+	exports jloda.fx.message;
+	exports jloda.fx.selection;
+	exports jloda.fx.shapes;
+	exports jloda.fx.undo;
+	exports jloda.fx.util;
+	exports jloda.fx.window;
+
+	opens jloda.fx.colorscale;
+	opens jloda.fx.label;
+	opens jloda.fx.control.table;
+	opens jloda.fx.control.sliderhistogram;
+	opens jloda.fx.find;
+	opens jloda.fx.message;
+
+	exports jloda.fx.geom;
+	exports jloda.fx.workflow;
+	exports jloda.fx.selection.rubberband;
+	exports jloda.fx.thirdparty;
+}
